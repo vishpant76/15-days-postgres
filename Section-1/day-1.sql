875d417aad7d36275cc7c182069df6e6e8a37402
Vishal Pant <vishal.pant1@gmail.com>
2024-04-16 11:26:18 +0530

    day-1 sql

--- Section-1/day-1.sql
+++ Section-1/day-1.sql
@@ -20,3 +20,52 @@ select first_name, last_name, email
 from customer
 order by 2 desc, 1 desc;
 
+
+-- DISTINCT
+select distinct 
+first_name, last_name
+from actor
+order by first_name;
+
+select distinct rating from film;
+
+-- distinct is across all of the selected columns
+select distinct rating, rental_duration from film;
+
+
+-- Challenge 3
+select distinct amount from payment
+order by amount desc;
+
+-- LIMIT
+-- Get the records having latest 10 rental dates
+select * from rental
+order by rental_date desc
+limit 10;
+
+-- COUNT
+select count(*)
+from rental;
+
+-- COUNT in combination with DISTINCT
+-- select count(first_name) from actor;
+select
+count (DISTINCT first_name)
+from actor;
+
+select count(distinct first_name) from customer;
+
+
+-- DAY 1 more Challenges --
+select distinct district from address;
+
+-- latest rental date
+select * from rental
+order by rental_date desc
+limit 1;
+
+-- How many films
+select count(*) from film;
+
+-- How many distinct last names of the customers
+select count(distinct last_name) from customer;
