f5495135c14be98efeff1e5db201b4de81045021
Vishal Pant <vishal.pant1@gmail.com>
2024-05-02 12:14:53 +0530

    day 11 window function updates

--- Section-11/day-11-sql.sql
+++ Section-11/day-11-sql.sql
@@ -1,1 +1,24 @@
 -- Section 11 - Day 11 - WIndow Functions
+-- select * from payment;
+select *, SUM(amount) OVER(PARTITION BY customer_id)
+from payment;
+
+-- COUNT
+-- This will show us how many transactions there were per customer.
+select *, count(*) OVER(PARTITION BY customer_id)
+from payment;
+
+-- We can even partition by multiple columns
+-- How many sales occurred for each staff ID per customer:
+select *, SUM(amount) OVER(PARTITION BY customer_id, staff_id)
+from payment;
+
+-- Using COUNT without any Paritition By
+select *, count(*) OVER() 
+FROM payment
+order by 1;
+
+-- Using ROUND with Avg when used as window function:
+select *, round(avg(amount) over(), 2)
+from payment
+order by 1;
