d889b3b5726d5f144bcc9f56bd0b402cb1dd24a7
Vishal Pant <vishal.pant1@gmail.com>
2024-05-09 13:05:09 +0530

    day 8 project - challenge 10 done

--- Section-8/day-8-project.sql
+++ Section-8/day-8-project.sql
@@ -116,20 +116,108 @@ Task: Create the overview of the sales to determine the from which city (we are 
 Question: What city is that and how much is the amount?
 Answer: Cape Coral with a total amount of 221.55
 */
--- select * from sales;
--- select * from customer;
--- select * from address;
--- select * from city;
+select * from sales;
+select * from customer;
+select * from payment;
+select * from address;
+select * from city;
 
 -- WRONG RESULT!
-select city, sum(price)
-from customer cu
-inner join sales s
-on cu.customer_id = s.customer_id
+-- select city, sum(price)
+-- from customer cu
+-- inner join sales s
+-- on cu.customer_id = s.customer_id
+-- inner join address a
+-- on a.address_id = cu.address_id
+-- inner join city ci
+-- on ci.city_id = a.city_id
+-- group by city
+-- order by 2 desc
+-- limit 1;
+
+-- THIS IS THE CORRECT ONE:
+select city, sum(amount) from payment p
+inner join customer c
+on c.customer_id = p.customer_id
 inner join address a
-on a.address_id = cu.address_id
+on c.address_id = a.address_id
 inner join city ci
 on ci.city_id = a.city_id
 group by city
-order by 2 desc
-limit 1;
+order by 2 desc;
+
+-- Challenge 8
+/*
+Level: Moderate to difficult
+Topic: JOIN & GROUP BY
+Task: Create an overview of the revenue (sum of amount) grouped by a column in the format "country, city".
+Question: Which country, city has the least sales?
+Answer: United States, Tallahassee with a total amount of 50.85.
+*/
+-- select concat(co.country, ', ', ci.city) from payment;
+-- select* from customer;
+-- select * from address;
+-- select * from city;
+-- select * from country;
+
+select concat(co.country, ', ', ci.city) as country_with_city, sum(amount) from payment p
+inner join customer cu
+on cu.customer_id = p.customer_id
+inner join address a
+on cu.address_id = a.address_id
+inner join city ci
+on ci.city_id = a.city_id
+inner join country co
+on ci.country_id = co.country_id
+group by 1
+order by 2;
+
+-- Challenge 9 -failed to solve
+/*
+Level: Difficult
+Topic: Uncorrelated subquery
+Task: Create a list with the average of the sales amount each staff_id has per customer.
+Question: Which staff_id makes on average more revenue per customer?
+Answer: staff_id 2 with an average revenue of 56.64 per customer.
+*/
+-- select * from payment;
+-- select staff_id, round(avg(amount), 2)
+-- from payment
+-- group by staff_id;
+select staff_id, round(avg (total),2 ) 
+from
+(select staff_id, customer_id, sum(amount) as total
+from payment
+group by staff_id, customer_id
+order by 2)
+group by staff_id;
+
+-- A simpler approach
+select staff_id, round(sum(amount) / count(distinct customer_id), 2) as avg
+from payment
+group by staff_id;
+
+-- Challenge 10
+/*
+Level: Difficult to very difficult
+Topic: EXTRACT + Uncorrelated subquery
+Task: Create a query that shows average daily revenue of all Sundays.
+Question: What is the daily average revenue of all Sundays?
+Answer: 1410.65
+*/
+-- select *, extract(dow from payment_date) from payment
+-- where extract(dow from payment_date)=0;
+
+-- select extract(dow from payment_date), sum(amount)
+-- from payment
+-- group by 1
+-- having extract(dow from payment_date)=0;
+
+select round(avg(total),2) from
+(
+select date(payment_date) as date, sum(amount) as total
+from payment
+-- where extract(dow from payment_date)=0
+group by date(payment_date)
+	)
+	where extract(dow from date)=0;
